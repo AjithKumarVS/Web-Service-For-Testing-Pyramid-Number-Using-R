63f25d84f1e1813ccb1a25e568a95f1f05aaf3d7
Ajith Kumar V S <avakkalagantisunilku@hawk.iit.edu>
2020-02-05 17:48:41 -0600

    R Function for Testing

---
+++ Check_For_Pyramid_Number.r
@@ -0,0 +1,48 @@
+#' @post /boolean
+#' @param string is Pyramid Word?
+#String Function
+function(string){
+  flag<-TRUE
+  count_numbers<-c()
+  letters<-c()
+  for (i in 1:(nchar(str))){
+    sp<-substr(str,i,i)
+    concate<-c(concate,sp)
+  }
+  old<-strsplit(concate," ")
+  new<-old
+  for(i in 1:(length(old))){
+    counter<-0
+    for(j in (i):(length(old))){
+      if(unlist(new[i]) == unlist(old[j])){
+        counter<-counter+1
+        if(i!=j){
+          old[j] <- "0"
+        }
+      }
+    }
+    if(counter!=0){
+      count_numbers<-c(count_numbers,counter)
+      letters<-c(letters,unlist(new[i]))
+    }
+  }
+  
+  count_numbers<-sort(count_numbers,decreasing=FALSE)
+  
+  #Checks If It Follows The Principle Of Pyramid Numbers
+  for(i in 1:(length(count_numbers)-1)){
+    if(count_numbers[1]==1){
+      if(count_numbers[i]+1!=count_numbers[i+1]){
+        flag<-FALSE
+      }
+    }
+  }
+  #Function Returns True If It Is A Pyramid Number
+  if(flag==TRUE){
+    return(TRUE)
+  }
+  else{
+    return(FALSE)
+  }
+}
+
